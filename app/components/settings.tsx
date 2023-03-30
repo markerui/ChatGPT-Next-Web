d1b540bb1b4398d289345e8c639295d9443f4f6e
masaiui@163.com <>
2023-03-30 10:55:54 +0800

    hidden other

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -313,7 +313,7 @@ export function Settings(props: { closeSettings: () => void }) {
           </SettingItem>
         </List>
         <List>
-          {enabledAccessControl ? (
+          {/* {enabledAccessControl ? (
             <SettingItem
               title={Locale.Settings.AccessCode.Title}
               subTitle={Locale.Settings.AccessCode.SubTitle}
@@ -329,7 +329,7 @@ export function Settings(props: { closeSettings: () => void }) {
             </SettingItem>
           ) : (
             <></>
-          )}
+          )} */}
 
           {/* <SettingItem
             title={Locale.Settings.Token.Title}
